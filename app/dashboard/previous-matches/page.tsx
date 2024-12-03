edc466da114ee3395eb6bef413545413ff9abf3d
davidnoq <davidnoq15@gmail.com>
2024-12-03 04:47:29 -0500

    profile in dashboard

---
+++ app/dashboard/previous-matches/page.tsx
@@ -0,0 +1,13 @@
+// app/dashboard/previous-matches/page.tsx
+import React from "react";
+
+const PreviousMatches: React.FC = () => {
+  return (
+    <div>
+      <h1 className="text-white text-3xl font-bold mb-4">Previous Matches</h1>
+      <p className="text-white">List of previous matches.</p>
+    </div>
+  );
+};
+
+export default PreviousMatches;
